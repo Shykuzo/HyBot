104909fbc921404138f974ac2bd228a4e01667ea
Shykuzo <Stroobants.Lucas@hotmail.com>
2022-07-10 15:27:31 +0200

    [+] Main Class | Register Listeners, ...

--- src/fr/hytaria_network/hybot/Main.java
+++ src/fr/hytaria_network/hybot/Main.java
@@ -1,8 +1,13 @@
 package fr.hytaria_network.hybot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.hytaria_network.hybot.listeners.BlockListener;
+import fr.hytaria_network.hybot.listeners.ChatListener;
+import fr.hytaria_network.hybot.listeners.ServerListener;
 import fr.hytaria_network.hybot.utils.ConfigManager;
+import fr.hytaria_network.hybot.utils.LogManager;
 import fr.mrcubee.annotation.spigot.config.ConfigAnnotation;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -11,8 +16,10 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Main extends JavaPlugin {
 	
 	private static Main instance;
-	private ConfigManager configManager;
 	private JDA discordAPI;
+	
+	private ConfigManager configManager;
+	private LogManager logManager;
 	
 	// ---------------------------------------- \\
 	
@@ -20,6 +27,7 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		instance = this;
 		configManager = new ConfigManager();
+		logManager = new LogManager();
 		
 		ConfigAnnotation.loadClass(getConfig(), getConfigManager());
 		saveDefaultConfig();
@@ -40,6 +48,23 @@ public class Main extends JavaPlugin {
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+		
+		Bukkit.getPluginManager().registerEvents(new ChatListener(), getInstance());
+		Bukkit.getPluginManager().registerEvents(new BlockListener(), getInstance());
+		Bukkit.getPluginManager().registerEvents(new ServerListener(), getInstance());
+	}
+	
+	@Override
+	public void onDisable() {
+		if(discordAPI != null) {
+			discordAPI.shutdown();
+		}
+	}
+	
+	@Override
+	public void reloadConfig() {
+		super.reloadConfig();
+		ConfigAnnotation.loadClass(getConfig(), getConfigManager());
 	}
 	
 	// ---------------------------------------- \\
@@ -62,6 +87,10 @@ public class Main extends JavaPlugin {
 	
 	public ConfigManager getConfigManager() {
 		return configManager;
+	}
+	
+	public LogManager getLogManager() {
+		return logManager;
 	}
 	
 }
